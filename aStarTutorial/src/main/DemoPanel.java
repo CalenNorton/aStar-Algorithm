62b1fd646d9ef57d3eceb0b8d88cb89027b8814a
CalenNorton <norton.calen@gmail.com>
2026-02-08 22:04:40 +1030

    Porting the project to Github
    Moving project from my local environment to Github for security

---
+++ aStarTutorial/src/main/DemoPanel.java
@@ -0,0 +1,257 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class DemoPanel extends JPanel {
+
+    // SCREEN SETTINGS
+    final int maxCol = 15;
+    final int maxRow = 10;
+    final int nodeSize = 70;
+    final int screenWidth = nodeSize * maxCol;
+    final int screenHeight = nodeSize * maxRow;
+
+    // NODE
+    Node[][] node =  new Node[maxCol][maxRow];
+    Node startNode, goalNode, currentNode;
+    ArrayList<Node> openList = new ArrayList<>();
+    ArrayList<Node> checkedList = new ArrayList<>();
+
+    // OTHERS
+    boolean goalReached = false;
+    int step  = 0;
+
+    public DemoPanel() {
+
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setLayout(new GridLayout(maxRow, maxCol));
+        this.addKeyListener(new KeyHandler(this));
+        this.setFocusable(true);
+
+        // PLACE NODES
+        int col = 0;
+        int row = 0;
+
+        while (col < maxCol && row < maxRow) {
+
+            node[col][row] = new Node(col, row);
+            this.add(node[col][row]);
+
+            col++;
+            if(col == maxCol) {
+                col = 0;
+                row++;
+            }
+
+        }
+
+        // SET START AND GOAL NODE
+        setStartNode(3, 6);
+        setGoalNode(11, 3);
+
+        //PLACE SOLID NODES
+        setSolidNode(10, 2);
+        setSolidNode(10, 3);
+        setSolidNode(10, 4);
+        setSolidNode(10, 5);
+        setSolidNode(10, 6);
+        setSolidNode(10, 7);
+        setSolidNode(6, 2);
+        setSolidNode(7, 2);
+        setSolidNode(8, 2);
+        setSolidNode(9, 2);
+        setSolidNode(10, 7);
+        setSolidNode(11, 7);
+        setSolidNode(12, 2);
+        setSolidNode(6, 1);
+
+        // SET COST
+        setCostOnNodes();
+
+    }
+
+    private void setStartNode(int col, int row) {
+        node[col][row].setAsStart();
+        startNode = node[col][row];
+        currentNode = startNode;
+    }
+
+    private void setGoalNode(int col, int row) {
+        node[col][row].setAsGoal();
+        goalNode = node[col][row];
+    }
+
+    private void setSolidNode(int col, int row) {
+        node[col][row].setAsSolid();
+
+    }
+
+    private void setCostOnNodes(){
+        int col = 0;
+        int row = 0;
+        while (col < maxCol && row < maxRow) {
+            getCost(node[col][row]);
+            col++;
+            if(col == maxCol) {
+                col = 0;
+                row++;
+            }
+        }
+
+    }
+
+    private void getCost(Node node) {
+
+        // GET G COST (DISTANCE FROM THE START NODE)
+        int xDistance = Math.abs(node.col - startNode.col);
+        int yDistance = Math.abs(node.row - startNode.row);
+        node.gCost = xDistance + yDistance;
+
+        // GET H COST (DISTANCE FROM THE GOAL NODE)
+        xDistance = Math.abs(node.col - goalNode.col);
+        yDistance = Math.abs(node.row - goalNode.row);
+        node.hCost = xDistance + yDistance;
+
+        // GET F COST (SUM OF G + H COST)
+        node.fCost = node.gCost + node.hCost;
+
+        // DISPLAY THE CONSTANT ON THE NODE
+        if(node != startNode &&  node != goalNode) {
+            node.setText("<html>F:" + node.fCost + "<br>G: " + node.gCost + "</html>");
+        }
+    }
+
+    public void search(){
+        if(goalReached == false && step < 300) {
+
+            int col = currentNode.col;
+            int row = currentNode.row;
+
+            currentNode.setAsChecked();
+            checkedList.add(currentNode);
+            openList.remove(currentNode);
+
+            // OPEN THE UP NODE
+            if(row - 1 >= 0){
+                openNode(node[col][row-1]);
+            }
+            // OPEN THE LEFT NODE
+            if(col - 1 >= 0){
+                openNode(node[col-1][row]);
+            }
+            // OPEN THE DOWN NODE
+            if(row + 1 < maxRow){
+                openNode(node[col][row+1]);
+            }
+            // OPEN THE RIGHT NODE
+            if(col + 1 < maxCol){
+                openNode(node[col+1][row]);
+            }
+
+            // FIND THE BEST NODES
+            int bestNodeIndex = 0;
+            int bestNodeCost = 999;
+            for(int i = 0; i < openList.size(); i++){
+
+                // CHECK IF THIS NODE'S F COST IS BETTER
+                if(openList.get(i).fCost < bestNodeCost){
+                    bestNodeIndex = i;
+                    bestNodeCost = openList.get(i).fCost;
+                }
+                // IF F COST IS EQUAL, USE G COST
+                else if(openList.get(i).fCost == bestNodeCost){
+                    if(openList.get(i).gCost < openList.get(bestNodeIndex).gCost){
+                        bestNodeIndex = i;
+                    }
+                }
+            }
+            // AFTER THE LOOP, WE GET THE BEST NODE WHICH IS OUR NEXT STEP
+            currentNode = openList.get(bestNodeIndex);
+
+            if(currentNode == goalNode){
+                goalReached = true;
+                trackThePath();
+            }
+        }
+        step++;
+    }
+
+    public void autoSearch(){
+        while(goalReached == false && step < 300 ) {
+
+            int col = currentNode.col;
+            int row = currentNode.row;
+
+            currentNode.setAsChecked();
+            checkedList.add(currentNode);
+            openList.remove(currentNode);
+
+            // OPEN THE UP NODE
+            if(row - 1 >= 0){
+                openNode(node[col][row-1]);
+            }
+            // OPEN THE LEFT NODE
+            if(col - 1 >= 0){
+                openNode(node[col-1][row]);
+            }
+            // OPEN THE DOWN NODE
+            if(row + 1 < maxRow){
+                openNode(node[col][row+1]);
+            }
+            // OPEN THE RIGHT NODE
+            if(col + 1 < maxCol){
+                openNode(node[col+1][row]);
+            }
+
+            // FIND THE BEST NODES
+            int bestNodeIndex = 0;
+            int bestNodeCost = 999;
+            for(int i = 0; i < openList.size(); i++){
+
+                // CHECK IF THIS NODE'S F COST IS BETTER
+                if(openList.get(i).fCost < bestNodeCost){
+                    bestNodeIndex = i;
+                    bestNodeCost = openList.get(i).fCost;
+                }
+                // IF F COST IS EQUAL, USE G COST
+                else if(openList.get(i).fCost == bestNodeCost){
+                    if(openList.get(i).gCost < openList.get(bestNodeIndex).gCost){
+                        bestNodeIndex = i;
+                    }
+                }
+            }
+            // AFTER THE LOOP, WE GET THE BEST NODE WHICH IS OUR NEXT STEP
+            currentNode = openList.get(bestNodeIndex);
+
+            if(currentNode == goalNode){
+                goalReached = true;
+                trackThePath();
+            }
+        }
+        step++;
+    }
+
+    private void openNode(Node node) {
+        if(node.open == false && node.checked == false && node.solid == false) {
+
+            // IF THE NODE IS NOT YET OPENED, ADD IT TO THE OPEN LIST
+            node.setAsOpen();
+            node.parent = currentNode;
+            openList.add(node);
+        }
+    }
+
+    private void trackThePath(){
+        // BACKTRACK AND DRAW THE BEST PATH
+        Node current = goalNode;
+        while(current != startNode){
+            current = current.parent;
+            if(current != startNode){
+                current.setAsPath();
+            }
+        }
+    }
+}
---
+++ aStarTutorial/src/main/KeyHandler.java
@@ -0,0 +1,34 @@
+package main;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+
+    DemoPanel dp;
+
+    public KeyHandler(DemoPanel dp){
+        this.dp = dp;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        //unused
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+        if(code == KeyEvent.VK_ENTER){
+            dp.search();
+        }
+        if(code == KeyEvent.VK_SPACE){
+            dp.autoSearch();
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        //unused
+    }
+}
